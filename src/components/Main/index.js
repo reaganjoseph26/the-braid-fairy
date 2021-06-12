1919d4da2c7d5dc4bfec83846e13a44ec1d553f9
Racer <reaganjoseph26@yahoo.com>
2021-06-12 02:23:16 -0400

    css changes

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 function Main() {
   return (
-    <section className="landing">
-      <div>
-        <img src={require(`../../assets/images/Braid_fairy-logo.jpg`).default} alt="logo"></img>
-      </div>
-    </section>
+    <main>
+      <section className="landing">
+        <div>
+          <img src={require(`../../assets/images/Braid_fairy-logo.jpg`).default} alt="logo"></img>
+        </div>
+      </section>
+
+      <div className="carousel"></div>
+
+      <div className="links-div">
+          <ul className="main-links">
+            <li><a href="">Book Appointment</a></li>
+            <li><a href="">Tutorial</a></li>
+            <li><a href="">Shop</a></li>
+          </ul>
+        </div>
+    </main>
   );
 }
 
