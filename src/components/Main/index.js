1d650e0f3eda695697a15a165c7bf43aed664318
Racer <reaganjoseph26@yahoo.com>
2021-06-11 23:58:11 -0400

    main html set up

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <section className="landing">
+      <div>
+        <img src={require(`../../assets/images/Braid_fairy-logo.jpg`).default} alt="logo"></img>
+      </div>
+    </section>
+  );
+}
+
+export default Main;
