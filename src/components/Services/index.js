71acfa7e862415e9f8b9a8f26b8b4534ff1f76e6
Racer <reaganjoseph26@yahoo.com>
2021-06-25 03:14:51 -0400

    card layout underway

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -3,29 +3,109 @@ import React from 'react';
 function Services() {
     return (
         <main>
-        <section className="landing">
-          <div>
-            <img src={require(`../../assets/images/logo.jpg`).default} alt="logo"></img>
-          </div>
-        </section>
-  
-        <div className="contact-info">
-          <p>Location: Orlando, Florida</p>
-          <p>Buisness Hours: Mon-Sun: 10:00am-7:00pm</p>
-          <p>Text Line: 0000000000</p>
-          <p>Booking avaiable online only</p>
-        </div>
-  
-        {/* <Carousel /> */}
-  
-        {/* <div className="links-div">
-            <ul className="main-links">
-              <li onClick={() => {props.setRenderedComponent("Services")}}><a href="#Services">Services</a></li>
-              <li><a href="">Tutorial</a></li>
-              <li><a href="">Shop</a></li>
-            </ul>
-          </div> */}
-      </main>
+            <section className="landing">
+            <div>
+                <img src={require(`../../assets/images/logo.jpg`).default} alt="logo"></img>
+            </div>
+            </section>
+
+            <h1 className="services">Services</h1>
+            
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="card text-center" style={{ width: '18rem' }}>
+                            <img className="card-img-top" src={require(`../../assets/images/test1.jpg`).default} alt=""></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Lace Bond-in</h5>
+                                <a href="#" className="card-link">Read More</a>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">35 min</li>
+                                <li className="list-group-item">$40</li>
+                            </ul>
+                            <div className="card-body">
+                                <a href="#" className="card-link">Book Now</a>
+                            </div>
+                        </div>                 
+                    </div>
+
+                    <div className="col-sm">
+                        <div className="card" style={{ width: '18rem' }}>
+                            <img className="card-img-top" src="..." alt=""></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Traditional Sew-In</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">Cras justo odio</li>
+                                <li className="list-group-item">Dapibus ac facilisis in</li>
+                                <li className="list-group-item">Vestibulum at eros</li>
+                            </ul>
+                            <div className="card-body">
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>                 
+                    </div>
+
+                    <div className="col-sm">
+                        <div className="card" style={{ width: '18rem' }}>
+                            <img className="card-img-top" src="..." alt=""></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Medium Braids</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">Cras justo odio</li>
+                                <li className="list-group-item">Dapibus ac facilisis in</li>
+                                <li className="list-group-item">Vestibulum at eros</li>
+                            </ul>
+                            <div className="card-body">
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>                 
+                    </div>
+
+                    <div className="col-sm">
+                        <div className="card" style={{ width: '18rem' }}>
+                            <img className="card-img-top" src="..." alt=""></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Medium Braids</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">Cras justo odio</li>
+                                <li className="list-group-item">Dapibus ac facilisis in</li>
+                                <li className="list-group-item">Vestibulum at eros</li>
+                            </ul>
+                            <div className="card-body">
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>                 
+                    </div>
+
+                    <div className="col-sm">
+                        <div className="card" style={{ width: '18rem' }}>
+                            <img className="card-img-top" src="..." alt=""></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Medium Braids</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">Cras justo odio</li>
+                                <li className="list-group-item">Dapibus ac facilisis in</li>
+                                <li className="list-group-item">Vestibulum at eros</li>
+                            </ul>
+                            <div className="card-body">
+                                <a href="#" className="card-link">Card link</a>
+                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>                 
+                    </div>
+               </div>
+        </main>
     );
 }
   
